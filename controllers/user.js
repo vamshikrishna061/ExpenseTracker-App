422fba8eeb0609439323d91ee563d75446d418f1
vamshikrishna061 <kandagatlavamshikrishna11>
2023-04-21 20:31:18 +0530

    logged in

---
+++ controllers/user.js
@@ -0,0 +1,33 @@
+const User = require("../models/user");
+
+exports.postUser = async (req, res, next) => {
+  if (req.body.name === "" && req.body.email == "" && req.body.pass == "") {
+    throw new Error("all the fields are mendatory");
+  }
+  try {
+    await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.pass,
+    }).then((user) => {
+      res.status(201).json(user);
+      console.log("user signed up");
+    });
+  } catch (err) {
+    res.status(500).json(err);
+    console.log("error in creating user");
+  }
+};
+
+exports.loginUser = (req, res, next) => {
+  let email = req.body.email;
+  let password = req.body.pass;
+  User.findAll({ where: { email: email, password: password } })
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+};
--- frontend/signup.js
+++ frontend/signup.js
@@ -1,30 +1,87 @@
-const signup = document.getElementById("signup");
+let signup = document.getElementById("signup");
 
 signup.addEventListener("click", (e) => {
   e.preventDefault();
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const pass = document.getElementById("password").value;
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var pass = document.getElementById("password").value;
+  //   if (name == "") {
+  //     let element = document.querySelector(".popup");
+  //     let n_element = document.createElement("div");
+  //     n_element.className = "toast";
+  //     n_element.innerText = "*name cannot empty";
+  //     element.appendChild(n_element);
+  //     setTimeout(() => {
+  //       n_element.remove();
+  //     }, 2000);
+  //     return;
+  //   }
+  //   if (email == "") {
+  //     let element = document.querySelector(".popup");
+  //     let n_element = document.createElement("div");
+  //     n_element.className = "toast";
+  //     n_element.innerText = "*email cannot empty";
+  //     element.appendChild(n_element);
+  //     setTimeout(() => {
+  //       n_element.remove();
+  //     }, 2000);
+  //     return;
+  //   }
+  //   if (pass == "") {
+  //     let element = document.querySelector(".popup");
+  //     let n_element = document.createElement("div");
+  //     n_element.className = "toast";
+  //     n_element.innerText = "*password cannot empty";
+  //     element.appendChild(n_element);
+  //     setTimeout(() => {
+  //       n_element.remove();
+  //     }, 2000);
+  //     return;
+  //   }
+  if (name == "") {
+    let n = "*name cannot empty";
+    popup(n);
+    return;
+  }
+  if (email == "") {
+    let e = "*email cannot empty";
+    popup(e);
+    return;
+  }
+  if (pass == "") {
+    let p = "*password cannot empty";
+    popup(p);
+    return;
+  }
+
   let user = { name, email, pass };
+
   axios
     .post("http://localhost:3000/user/signup", user)
     .then((user) => {
-      console.log(user.data);
+      let response = user.data.message;
+      popup(response);
+      if (user.data.success == true) {
+        window.location.href = "./login.html";
+      }
     })
     .catch((err) => {
       console.log(err);
-      let error=err.response.data.original.code;
-    if(error=='ER_DUP_ENTRY')
-    {
-      let element=document.querySelector('.popup');
-      let n_element=document.createElement('div');
-      n_element.className='toast';
-      n_element.innerText='*email already registered';
-      element.appendChild(n_element);
-      setTimeout(()=> {
-        n_element.remove();
-        },2000); 
-        return;
-    }
+      let error = err.response.data.original.code;
+      if (error == "ER_DUP_ENTRY") {
+        let errRes = "*email already registered";
+        popup(errRes);
+      }
     });
 });
+
+function popup(data) {
+  let element = document.querySelector(".popup");
+  let n_element = document.createElement("div");
+  n_element.className = "toast";
+  n_element.innerText = data;
+  element.appendChild(n_element);
+  setTimeout(() => {
+    n_element.remove();
+  }, 2000);
+}
